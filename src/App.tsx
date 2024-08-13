501e683c0c1d6a2ed3adc4db100613ae7685f975
KrhystFerrari <krhyst_ferrari@hotmail.com>
2024-08-13 18:54:31 -0300

    feat: escopo do formulario de cadastro criado

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import Form from "./components/Form"
+import Form from "./components/Form";
 
 function App() {
-
   return (
-    <>
+    <div>
       <h1 className="text-red-600">Inscreva-se</h1>
-      <Form />
-    </>
-  )
+      <p>Assine nossa Newsletter e mantenha-se informado!</p>
+      <div>
+        <Form />
+      </div>
+      <p>
+        Ao se inscrever, você passará a receber os nossos e-mails com as
+        melhores dicas e novidades.{" "}
+      </p>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,24 @@
-
 const Form = () => {
   return (
-    <div>Form</div>
-  )
-}
+    <form>
+      <div>
+        <label>Nome: </label>
+        <input type="text" placeholder="Digite seu nome" />
+      </div>
+      <div>
+        <label>Email: </label>
+        <input type="email" placeholder="Digite seu melhor e-mail" />
+      </div>
+      <div>
+        <a href="#">Leia os termos</a>
+        <div>
+          <input type="checkbox" />
+          <label>Concordo com os termos</label>
+        </div>
+      </div>
+      <button>Cadastrar</button>
+    </form>
+  );
+};
 
-export default Form
+export default Form;
